550813512f314034987eb69204643159b3af2d17
zalakir <olivier.valcourt@hotmail.com>
2023-12-05 18:49:33 -0500

    UpdateFrontNavbar

--- resources/views/front/layout.blade.php
+++ resources/views/front/layout.blade.php
@@ -6,6 +6,9 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ config('app.name') }}</title>
     @vite(['resources/css/front.css', 'resources/js/front.js'])
+    <!--Link-->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap">
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css" />
 </head>
 <body>
     <!-- Navbar -->
--- resources/views/front/navbar.blade.php
+++ resources/views/front/navbar.blade.php
@@ -1,4 +1,24 @@
 <!-- Navbar content here -->
-<nav>
-    <!-- Navbar links, etc. -->
-</nav>
+<nav class="nav">
+    <i class="uil uil-bars navOpenBtn"></i>
+    <ul class="logotxt">
+        <img src="img/logo_blank.png" class="logoimg">
+        <a href="#" class="logo">RaidSmith</a>
+    </ul>
+
+    <ul class="nav-links">
+        <i class="uil uil-times navCloseBtn"></i>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Products</a></li>
+        <li><a href="#">About Us</a></li>
+        <li><a href="#">Contact Us</a></li>
+    </ul>
+
+    <!-- Seachtoggle if needed later -->
+    <!--i class="uil uil-search search-icon" id="searchIcon"></i>
+    <div class="search-box">
+        <i class="uil uil-search search-icon"></i>
+        <input type="text" placeholder="Search here..." />
+    </div-->
+</nav>    
